62531e56ccb8aaa24f31a522daaa2e6a2d73a9be
Nathan Andrus <nathanpandrus@gmail.com>
2019-02-12 12:11:44 -0700

    Initial Commit

---
+++ DeckOfOneCardObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckOfOneCardObjectiveC
+//
+//  Created by Nathan Andrus on 2/12/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckOfOneCardObjectiveC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DeckOfOneCardObjectiveC
+//
+//  Created by Nathan Andrus on 2/12/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
